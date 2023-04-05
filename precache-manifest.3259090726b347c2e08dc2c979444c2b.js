0c0bfba9ba1813393728f8f33794970422cec6aa
ditholease <ditholease@gmail.com>
2023-04-05 05:58:18 +0000

    Updates

---
+++ precache-manifest.3259090726b347c2e08dc2c979444c2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "55ddf0557a2e6eafc857",
+    "url": "/CryptoExchangeDapp/static/css/main.0b06e3ee.chunk.css"
+  },
+  {
+    "revision": "55ddf0557a2e6eafc857",
+    "url": "/CryptoExchangeDapp/static/js/main.55ddf055.chunk.js"
+  },
+  {
+    "revision": "fc5c56b98a0ec343033a",
+    "url": "/CryptoExchangeDapp/static/css/1.87890d9c.chunk.css"
+  },
+  {
+    "revision": "fc5c56b98a0ec343033a",
+    "url": "/CryptoExchangeDapp/static/js/1.fc5c56b9.chunk.js"
+  },
+  {
+    "revision": "3fc4e6fe425fddaf371e",
+    "url": "/CryptoExchangeDapp/static/js/runtime~main.3fc4e6fe.js"
+  },
+  {
+    "revision": "c06ccd6b67a3f32fbc2cfa138d1c07a6",
+    "url": "/CryptoExchangeDapp/static/media/SASILogo.c06ccd6b.png"
+  },
+  {
+    "revision": "bd758393e0e81722f83eccbf808c6d7a",
+    "url": "/CryptoExchangeDapp/static/media/ethLogo_.bd758393.png"
+  },
+  {
+    "revision": "9a378999ee3e5e2997fd6af1cf7eeb11",
+    "url": "/CryptoExchangeDapp/index.html"
+  }
+];
